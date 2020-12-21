553f194c944907f5ed0dc4db7b54b6f7e5fcee14
Kamil <36671925+SawickiKamil@users.noreply.github.com>
2020-12-21 13:30:07 +0100

    Update state.js

--- src/state/state.js
+++ src/state/state.js
@@ -2,8 +2,25 @@ import headerData from './headerData'
 import mainData from './mainData'
 import footerData from './footerData'
 
+// interface IHeader: {
+//  name: string 
+//  year: string
+// }
 
-const state = {
+// type IHeaderResponse = IHeader[]
+
+// interface IStateResponse {
+//   headerData: IHeaderResponse
+// }
+
+
+// const state: IStateResponse = {
+//   headerData,
+//   mainData,
+//   footerData
+// }
+
+const state: IStateResponse = {
   headerData,
   mainData,
   footerData
